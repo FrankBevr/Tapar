794bee3109db70b83c09e9a8983fe9c924e6c174
FrankBevr <frank_dierolf@web.de>
2023-09-03 10:30:47 +0200

    :zap: (./WebComponent) add basic setup

--- WebComponent/src/App.vue
+++ WebComponent/src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-// create component
+import CoinTracker from "./components/coin-tracker.vue";
 </script>
 
 <template>
   <h1>Hello World</h1>
+  <CoinTracker></CoinTracker>
 </template>
 
 <style></style>
+// 1. create component // pass in some styles // pass in crypto values // styles
+// 2. Build web component // 3. Create brand HTML file // a. import web
+component
---
+++ WebComponent/src/components/coin-tracker.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h3>This is Cointracker</h3>
+</template>
